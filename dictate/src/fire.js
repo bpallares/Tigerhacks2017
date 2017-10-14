8871e22a0fc58ca1dbb206527be3e1620a7d870c
Boris Pallares <borispallares@hotmail.com>
2017-10-14 00:39:36 -0500

    Firebase addition

---
+++ dictate/src/fire.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase'
+var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
+  apiKey: 'AIzaSyDeOoXMfCIt2-WXJ6mLp3TcCQxcGK-lp38',
+  authDomain: 'dictate-244d5.firebaseapp.com',
+  databaseURL: 'https://dictate-244d5.firebaseio.com',
+  storageBucket: 'dictate-244d5'
+}
+const fire = firebase.initializeApp(config)
+export default fire
